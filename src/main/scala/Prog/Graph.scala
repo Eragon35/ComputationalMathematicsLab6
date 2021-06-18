80de64fe26e0245fe2120f65efebfcda21624afe
Eragon35 <antipin35@yahoo.com>
2021-06-18 12:32:56 +0300

    Added showing graphs

--- src/main/scala/Prog/Graph.scala
+++ src/main/scala/Prog/Graph.scala
@@ -3,7 +3,6 @@ package Prog
 import scalax.chart.module.Charting._
 
 object Graph {
-  def show(euler: IndexedSeq[(Double, Double)], milnes: IndexedSeq[(Double, Double)]): Unit = {
-
-  }
+  def show(euler: IndexedSeq[(Double, Double)], adams: IndexedSeq[(Double, Double)]): Unit =
+    XYLineChart(IndexedSeq(("Метод Эйлера", euler),("Метод Адамса", adams)).toXYSeriesCollection()).show("График функции", scrollable = true)
 }
