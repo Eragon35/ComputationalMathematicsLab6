ae4741313422c3ef8bde10019fef958997689ac5
Eragon35 <antipin35@yahoo.com>
2021-06-28 13:07:04 +0300

    Now calculate deviation & rework printing results for Adams

--- src/main/scala/Prog/Adams.scala
+++ src/main/scala/Prog/Adams.scala
@@ -1,13 +1,12 @@
 package Prog
 
 import java.lang.Math.pow
-import scala.annotation.tailrec
 
 object Adams {
-  def solve(equation: Equation, h: Double): IndexedSeq[(Double, Double)] = {
-    var data = euler(equation, h)
+  def solve(equation: Equation, h: Double, recursion: Boolean = true): IndexedSeq[(Double, Double)] = {
+    var (data, lines) = euler(equation, h)
     var xi: Double = data.last._1 + h
-    var answer = "Метод Адамса:\ni   xi     yi      f()      точное значение\n"
+//    var lines = IndexedSeq[String]()
     // Yi+1 = Yi + h * Fi + h^2/2 * ΔFi + 5h^3/12 * Δ^2Fi + 3h^4/8 * Δ^3Fi
     while (xi < equation.end + .00001) {
       val Fmin3 = equation.func(data(data.size - 4)._1, data(data.size - 4)._2)
@@ -18,20 +17,32 @@ object Adams {
       val delta2h = 5 / 12 * pow(h, 3) * (Fi - 2 * Fmin1 + Fmin2) // 5h^3/12 * Δ^2Fi
       val delta3 = Fi - 3 * Fmin1 + 3 * Fmin2 - Fmin3 // Δ^3Fi
       val yi = data.last._2 + h * Fi + delta1h + delta2h + 3 / 8 * pow(h, 4) * delta3 // Yi+1
+      // calculate R
       if (Math.abs(yi - equation.solution(xi)) > equation.accuracy) return solve(equation, h / 2)
-      answer += f"${((xi - equation.start._1) / h + 0.1).toInt}  $xi%1.2f  " +
-        f"$yi%1.4f  ${equation.func(xi, yi)}%1.4f   ${equation.solution(xi)}%1.4f\n"
+      lines = lines :+ f"${((xi - equation.start._1) / h + 0.1).toInt}  $xi%1.2f  " +
+        f"$yi%1.4f  ${equation.func(xi, yi)}%1.4f   ${equation.solution(xi)}%1.4f"
       data = data :+ (xi, yi)
       xi += h
     }
-    println(answer)
+    if (recursion) {
+      val y2h = solve(equation, h / 2 , recursion = false)
+      val r = y2h.zipWithIndex.filter { case (_, i) => (i + 1) % 2 != 0 }.map { case (v, _) => v}
+      var answer = "Метод Адамса:\n\t\t\t\t\t\t  точное\ni   xi     yi      f()   значение погрешность\n"
+      for (i <- lines.indices) answer += lines(i) + f"   ${(data(i)._2 - r(i)._2) / 15}%1.4f\n"
+      println(answer)
+    }
     data
   }
 
-  private def euler(equation: Equation, h: Double): IndexedSeq[(Double, Double)] = {
+  private def euler(equation: Equation, h: Double): (IndexedSeq[(Double, Double)], IndexedSeq[String]) = {
     var answer = IndexedSeq[(Double, Double)]((equation.start._1, equation.start._2))
-    for (i <- 1 until 4) answer  = answer :+ (equation.start._1 + h * i,
-      answer.last._2 + h * equation.func(answer.last._1, answer.last._2))
-    answer
+    var lines = IndexedSeq[String]()
+    for (i <- 1 until 4) {
+      val yi = answer.last._2 + h * equation.func(answer.last._1, answer.last._2)
+      lines = lines :+ f"$i  ${equation.start._1 + h * i}%1.2f  $yi%1.4f  " +
+        f"${equation.func(answer.last._1, answer.last._2)}%1.4f   ${equation.solution(equation.start._1 + h * i)}%1.4f"
+      answer = answer :+ (equation.start._1 + h * i, yi)
+    }
+    (answer, lines)
   }
 }
--- src/main/scala/Prog/Euler.scala
+++ src/main/scala/Prog/Euler.scala
@@ -1,20 +1,23 @@
 package Prog
 
-import scala.annotation.tailrec
 
 object Euler {
   def solve(equation: Equation, h: Double): IndexedSeq[(Double, Double)] = {
     var y0 = equation.start._2
     var xi: Double = equation.start._1
     var data = IndexedSeq[(Double, Double)]((xi, y0))
-    var answer = "Метод Эйлера:\ni   xi     yi      f()      точное значение\n"
+    var answer = "Метод Эйлера:\n\t\t\t\t\t\t  точное\ni   xi     yi      f()   значение погрешность\n"
     // Yi+1 = Yi + h * F(Xi, Yi)
     while (xi + h < equation.end + .00001) {
       val yi = y0 + h * equation.func(xi, y0)
       xi += h
+      // calculate R
+      val y0r = y0 + h / 2 * equation.func(xi, y0)
+      val yir = y0r + h / 2 * equation.func(xi + h / 2, y0r)
+      val r = Math.abs(yi - yir) / (2 - 1)
       if (Math.abs(yi - equation.solution(xi)) > equation.accuracy) return solve(equation, h / 2)
       answer += f"${((xi - equation.start._1) / h + 0.1).toInt}  $xi%1.2f  " +
-        f"$yi%1.4f  ${equation.func(xi, y0)}%1.4f   ${equation.solution(xi)}%1.4f\n"
+        f"$yi%1.4f  ${equation.func(xi, y0)}%1.4f   ${equation.solution(xi)}%1.4f   $r%1.4f\n"
       data = data :+ (xi, yi)
       y0 = yi
     }
